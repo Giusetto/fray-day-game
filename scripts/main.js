42cf8b07c8c51c29bdb31d212bd569f11572a5a8
Giusetto <55229659+Giusetto@users.noreply.github.com>
2019-09-20 12:13:05 +0200

    cccc

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 
 const cuby = document.querySelector(".cuby");
-let postop = 10;
-let posleft = 10;
+let postop = 0;
+let posleft = 0;
 let strpos = "";
 let speed = 50;
 const mainElement = document.querySelector("main");
@@ -16,6 +16,20 @@ const cubyWidth = +cubyStyle.width.slice(0,-2);
 
 
 document.addEventListener("keyup", move);
+
+cubyElement.addEventListener("click", touch);
+
+
+function touch (event) {
+    cubyElement.style.backgroundColor="red";
+    document.removeEventListener("keyup", move);
+    cubyElement.removeEventListener("click", touch);
+    alert("You win");
+    
+}
+
+
+
 
 
 
@@ -35,20 +49,26 @@ function move(e) {
             break;
             case 38: // up
             postop -= speed;
+            if (postop < 0 ){
+                postop = mainHeight - cubyHeight;
+            }
             strpos = postop + "px";
-            //
             cuby.style.top = strpos;
             break;
             case 39: // right
             posleft += speed;
+            if ((posleft + cubyWidth) >= mainWidth ){
+                posleft = 0;
+            }
             strpos = posleft + "px";
-            //
             cuby.style.left = strpos;
             break;
             case 37: // left
             posleft -= speed;
+            if (posleft < 0 ){
+                posleft = mainWidth - cubyWidth;
+            }
             strpos = posleft + "px";
-            //
             cuby.style.left = strpos;
             break;
             default:
