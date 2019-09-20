d3d80050db94f445437963b769a97265f462b7be
ozo7 <send2olaf@yahoo.de>
2019-09-20 11:22:44 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,18 @@
 const cuby = document.querySelector(".cuby");
 let postop = 10;
 let posleft = 10;
-//let strpos = "";
+let strpos = "";
 let speed = 50;
+const mainElement = document.querySelector("main");
+const mainStyle = getComputedStyle(mainElement);
+const mainHeight = +mainStyle.height.slice(0,-2);
+const mainWidth = +mainStyle.width.slice(0,-2);
+
+const cubyElement = document.querySelector(".cuby");
+const cubyStyle = getComputedStyle(cubyElement);
+const cubyHeight = +cubyStyle.height.slice(0,-2);
+const cubyWidth = +cubyStyle.width.slice(0,-2);
+
 
 document.addEventListener("keyup", move);
 
@@ -11,30 +21,37 @@ document.addEventListener("keyup", move);
 
 
 function move(e) {
-    console.log(e.keyCode);
+    //console.log(e.keyCode);
 
+    
     switch (e.keyCode) {
         case 40: // down
-            postop += speed;
-            console.log(postop);
-            strpos = postop + "px";
+            postop += speed;            
+            if ((postop + cubyHeight) >= mainHeight ){
+                postop = 0;
+            }
+            strpos = postop + "px";        
             cuby.style.top = strpos;
             break;
-        case 38: // up
+            case 38: // up
             postop -= speed;
             strpos = postop + "px";
+            //
             cuby.style.top = strpos;
             break;
-        case 39: // right
+            case 39: // right
             posleft += speed;
             strpos = posleft + "px";
+            //
             cuby.style.left = strpos;
             break;
-        case 37: // left
+            case 37: // left
             posleft -= speed;
             strpos = posleft + "px";
+            //
             cuby.style.left = strpos;
             break;
-        default:
-    }
+            default:
+            }
+   
 }
